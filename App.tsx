a07befd69f378edec076f6b0127101d868210491
Montso-Matlaletsa <montso.matlaletsa@gmail.com>
2022-07-28 20:29:34 +0200

    bla bla

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React, {useEffect} from 'react';
+import {View, Button} from 'react-native';
+import crashlytics from '@react-native-firebase/crashlytics';
+
+export default function App() {
+  useEffect(() => {}, []);
+  const sign = () => {
+    crashlytics().log('TESTING A CRASH');
+  };
+
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+      }}>
+      <Button title="Test Crash" />
+      <Button title="Sign In" />
+    </View>
+  );
+}
